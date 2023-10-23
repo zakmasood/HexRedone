0f6c8f2a43ef2d0e3656e1e6264aefd06c451bfa
Zakariya Azzam Masood <zakalimasood@gmail.com>
2023-10-24 01:33:28 +0500

    Init Push of Main Build

---
+++ HexRedone/Assets/Raycasting.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Raycasting : MonoBehaviour
+{
+    public GameObject camera;
+    public GameObject building;
+
+    public GameObject mining; // Particle effect
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left mouse button click
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject clickedTile = hit.collider.gameObject;
+
+                if (clickedTile.CompareTag("Tile")) // Make sure to set the tag on your tiles in the Unity Editor
+                {
+                    WorldGen worldGen = camera.GetComponent<WorldGen>();
+
+                    // Get neighbors of tile
+
+                    for (int i = 0; i < worldGen.gridLength; i++)
+                    {
+                        for (int j = 0; j < worldGen.gridWidth; j++)
+                        {
+                            if (worldGen.tiles[i, j].name == hit.collider.gameObject.name)
+                            {
+                                // Leveling for multitile buildings
+                                /* TODO:
+                                 * First grab the height of the clicked tile
+                                 * Then set the height of the neighboring tiles to the height of the center tile
+                                 */
+
+                                GameObject centerTile = worldGen.tiles[i, j].gameObject;
+
+                                if (i % 2 == 1)
+                                {
+                                    Debug.LogWarning("ODD ROW");
+
+                                    GameObject leftTile = worldGen.tiles[i, j + 1].gameObject;
+                                    GameObject rightTile = worldGen.tiles[i, j - 1].gameObject;
+
+                                    GameObject topLeftTile = worldGen.tiles[i + 1, j + 1].gameObject; // Top Right Odd
+                                    GameObject topRightTile = worldGen.tiles[i + 1, j].gameObject;
+
+                                    GameObject bottomLeftTile = worldGen.tiles[i - 1, j + 1].gameObject;
+                                    GameObject bottomRightTile = worldGen.tiles[i - 1, j].gameObject;
+
+                                    topRightTile.transform.position = new Vector3(topRightTile.transform.position.x, centerTile.transform.position.y, topRightTile.transform.position.z);
+                                    topLeftTile.transform.position = new Vector3(topLeftTile.transform.position.x, centerTile.transform.position.y, topLeftTile.transform.position.z);
+
+                                    bottomLeftTile.transform.position = new Vector3(bottomLeftTile.transform.position.x, centerTile.transform.position.y, bottomLeftTile.transform.position.z);
+                                    bottomRightTile.transform.position = new Vector3(bottomRightTile.transform.position.x, centerTile.transform.position.y, bottomRightTile.transform.position.z);
+
+                                    leftTile.transform.position = new Vector3(leftTile.transform.position.x, centerTile.transform.position.y, leftTile.transform.position.z);
+                                    rightTile.transform.position = new Vector3(rightTile.transform.position.x, centerTile.transform.position.y, rightTile.transform.position.z);
+                                }
+                                else
+                                {
+                                    Debug.LogWarning("EVEN ROW");
+
+                                    GameObject leftTile = worldGen.tiles[i, j + 1].gameObject;
+                                    GameObject rightTile = worldGen.tiles[i, j - 1].gameObject;
+
+                                    GameObject topLeftTile = worldGen.tiles[i + 1, j].gameObject;
+                                    GameObject topRightTile = worldGen.tiles[i + 1, j - 1].gameObject;
+
+                                    GameObject bottomLeftTile = worldGen.tiles[i - 1, j].gameObject;
+                                    GameObject bottomRightTile = worldGen.tiles[i - 1, j - 1].gameObject;
+
+                                    topRightTile.transform.position = new Vector3(topRightTile.transform.position.x, centerTile.transform.position.y, topRightTile.transform.position.z);
+                                    topLeftTile.transform.position = new Vector3(topLeftTile.transform.position.x, centerTile.transform.position.y, topLeftTile.transform.position.z);
+
+                                    bottomLeftTile.transform.position = new Vector3(bottomLeftTile.transform.position.x, centerTile.transform.position.y, bottomLeftTile.transform.position.z);
+                                    bottomRightTile.transform.position = new Vector3(bottomRightTile.transform.position.x, centerTile.transform.position.y, bottomRightTile.transform.position.z);
+
+                                    leftTile.transform.position = new Vector3(leftTile.transform.position.x, centerTile.transform.position.y, leftTile.transform.position.z);
+                                    rightTile.transform.position = new Vector3(rightTile.transform.position.x, centerTile.transform.position.y, rightTile.transform.position.z);
+                                }
+                            }
+                        }
+                    }
+                    // Building Placement
+
+                    /*for (int i = 0; i < worldGen.gridLength; i++)
+                    {
+                        for (int j = 0; j < worldGen.gridWidth; j++)
+                        {
+                            if (worldGen.tiles[i, j].name == hit.collider.gameObject.name)
+                            {
+                                // String to string comparasion, highly inefficient but you find a better method matej.
+
+                                Debug.Log("Found tile with name: " + hit.collider.gameObject.name + " | At Pos: " + i.ToString() + " " + j.ToString());
+                                // Building placement 
+                                GameObject newBuilding = Instantiate(building, new Vector3((float)hit.collider.gameObject.transform.position.x, (float)hit.collider.gameObject.transform.position.y + 0.28f, (float)hit.collider.gameObject.transform.position.z), Quaternion.identity);
+                                newBuilding.transform.Rotate(0, 30, 0); // Fix 90 degree bug
+                                worldGen.tiles[i, j].transform.Find("HB").GetComponent<TMP_Text>().text = "YES";
+                            }
+                        }
+                    }*/ 
+                }
+                else if (clickedTile.CompareTag("StoneTile"))
+                {
+                    Debug.Log("Stone Clicked!");
+                    GameObject pSystem = Instantiate(mining, new Vector3(clickedTile.transform.position.x, clickedTile.transform.position.y, clickedTile.transform.position.z), Quaternion.identity);
+                    pSystem.transform.Rotate(-90, 0, 0);
+                    pSystem.GetComponent<ParticleSystem>().Play();
+                }
+            }
+        }
+    }
+}
---
+++ HexRedone/Assets/WorldGen.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using TMPro;
+
+public class WorldGen : MonoBehaviour
+{
+    /*  Possiblity to use a custom scripted tool to generate tile prefabs?
+     *  EG: Add prefab model, add offset, add tag, and it would autogenerate the data in an array 🤔
+     */
+
+    public GameObject baseTilePrefab;
+    public GameObject stoneTilePrefab;
+
+    public int gridLength = 5;
+    public int gridWidth = 5;
+
+    public double horizontalOffset;
+    public double verticalOffset;
+
+    public GameObject[,] tiles; // Declare a 2D array to store the tiles (BASE)
+    public GameObject[,] resources; // 2D array to store the resource tiles (RESOURCES)
+
+    /*  With Resources array, no need to declare multiple, since we can use tag
+     *  comparisons to determine what resources are at the given location.
+     *  This also means we save on runtime performance by not using multiple array calls
+     *  and declarations.
+     */
+
+    [Header("Perlin Noise Generator")]
+    public double pHeight;
+    public double pWidth;
+
+    public Vector3 spriglet(float maxX, float maxZ, float height) // Randomizes pos
+    {
+        return new Vector3(maxX + Random.Range(-0.5f, 0.5f), height + 1.2f, maxZ + Random.Range(-0.5f, 0.5f));
+    }
+
+    public float[,] GenerateNoiseMap(int width, int height, float scale)
+    {
+        float[,] noiseMap = new float[width, height];
+
+        // Loop through each point in the grid
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                // Calculate sample coordinates based on scale and position
+                float sampleX = (float)x / width * scale;
+                float sampleY = (float)y / height * scale;
+
+                // Generate Perlin noise value at the sample coordinates
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+
+                // Store the noise value in the noise map
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+
+        return noiseMap;
+    }
+
+    void Start()
+    {
+        float[,] noiseMap = GenerateNoiseMap(gridWidth, gridLength, 5f);
+        tiles = new GameObject[gridLength, gridWidth];
+        resources = new GameObject[gridLength, gridWidth];
+
+        for (int q = 0; q < gridLength; q++)
+        {
+            for (int r = 0; r < gridWidth; r++)
+            {
+                double X = 1.5 * q;
+                double Z = 1.732 * (r + 0.5 * (q % 2));
+
+                // Grab perlin point
+                float perlinValue = noiseMap[q, r];
+                float height = perlinValue * 7 + 5;
+
+                GameObject baseTile = Instantiate(baseTilePrefab, new Vector3((float)X, height, (float)Z), Quaternion.identity);
+
+                baseTile.transform.Find("Coords").GetComponent<TMP_Text>().text = q.ToString() + ", " + r.ToString();
+                baseTile.transform.Rotate(0, 90, 0); // Fix 90 degree bug
+
+                int tileID = q * gridWidth + r;
+                baseTile.name = "Tile_" + tileID;
+
+                baseTile.transform.Find("TileID").GetComponent<TMP_Text>().text = baseTile.name;
+
+                float baseTileX = baseTile.transform.position.x;
+                float baseTileZ = baseTile.transform.position.z;
+
+                // Grass position randomization
+                baseTile.transform.Find("GrassOne").gameObject.transform.position = new Vector3(baseTileX + Random.Range(-0.5f, 0.5f), height + 1.2f , baseTileZ + Random.Range(-0.5f, 0.5f));
+                baseTile.transform.Find("GrassTwo").gameObject.transform.position = new Vector3(baseTileX + Random.Range(-0.5f, 0.5f), height + 1.2f , baseTileZ + Random.Range(-0.5f, 0.5f));
+                baseTile.transform.Find("GrassThree").gameObject.transform.position = new Vector3(baseTileX + Random.Range(-0.5f, 0.5f), height + 1.2f , baseTileZ + Random.Range(-0.5f, 0.5f));
+
+                tiles[q, r] = baseTile;
+
+                if (Random.Range(0, 4) == 2)
+                {
+                    GameObject stoneTile = Instantiate(stoneTilePrefab, new Vector3((float)X, baseTile.transform.position.y + 1f, (float)Z), Quaternion.identity);
+                    stoneTile.transform.Rotate(-90, 30, -105); // Fix 90 degree bug
+
+                    int stoneTileID = q * gridWidth + r;
+                    stoneTile.name = "StoneTile_" + stoneTileID;
+
+                    resources[q, r] = stoneTile;
+
+                    // Now we remove the grass at that tile
+
+                    tiles[q, r].transform.Find("GrassOne").gameObject.SetActive(false);
+                    tiles[q, r].transform.Find("GrassTwo").gameObject.SetActive(false);
+                    tiles[q, r].transform.Find("GrassThree").gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
